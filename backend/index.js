0c593150bb9217d0a3eda72911141b853fc344a4
anoopghm <140532297+anoopghm@users.noreply.github.com>
2024-12-30 11:34:14 +0545

    Login and food add done

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,195 @@
-const express = require('express'); 
-const cors = require('cors');
-const app = express(); 
+require("dotenv").config();
+
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString); 
+
+const User = require("./model/user.model");
+const Food = require("./model/food.model");
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
+
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: "*"
+    })
+);
+app.get("/",(req,res) => {
+    res.json({data: "hello"});
+});
+
 
 const port = 3000;
 
 app.use(cors()); 
 app.use(express.json());
+ 
 
-const foods = [
-    { name: "Roti", image: {}, price : 15 , description:"Made up of wheat, good source of carbohydarate." },
-    { name: "Pizza", image: {}, price : 250 , description:"Tasty cheesy pizza with oragano" },
-    { name: "Burger", image: {}, price : 150 , description:"Burger with wheat bun and home made steak" },
-    { name: "French Fries", image: {}, price : 80 , description:"Crispy french fries made in fresh oil, served with rosemary salt" },
-];
 
-app.get('/fooditems', (req, res) => {
-   res.json(foods); 
+// create-Account 
+
+app.post("/create-account",async (req,res) =>{
+  const {fullName, email, password} = req.body;
+
+  if(!fullName){
+      return res
+        .status(400)
+        .json({error: true, message : "Full Name is required" });
+  }
+
+  if(!email){
+      return res.status(400).json({error: true, message: "Email is required"});
+  }
+
+  if(!password){
+    return res
+    .status(400)
+    .json({error: true, message: "Password is required"});
+  }
+
+  const isUser = await User.findOne({email : email});
+
+  if(isUser){
+      return res.json({
+          error: true,
+          message : "User already exist",
+      });
+  }
+
+  const user = new User({
+      fullName,
+      email,
+      password,
+  });
+
+ await user.save();
+
+const accessToken = jwt.sign({ user}, process.env.ACCESS_TOKEN_SECRET, {
+  expiresIn: "3600m",
 });
+
+return res.json({
+  error:false,
+  user,
+  accessToken,
+  message: "Registration Successful",
+});
+
+});
+
+app.post("/login",async(req,res) => {
+  const{email,password} = req.body;
+  if(!email){
+     return res.status(400).json({message:"Email is required"});
+  }
+
+  if(!password){
+     return res.status(400).json({message:"Password is required"});
+  }
+
+  const userInfo = await User.findOne({email:email});
+
+  if(!userInfo) {
+     return res.status(400).json({message:"User not found"});
+  }
+
+  if(userInfo.email == email && userInfo.password == password){
+     const user = {user: userInfo };
+     const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+         expiresIn: "36000m",
+     });
+     return res.json({
+         error:false,
+         message : "Login Successful",
+         email,
+         accessToken,
+     });
+  }
+  else{
+     return res.status(400).json({
+         error:true,
+         message: "Invalid Credentials",
+     });
+  }
+ });
+
+ // Get User
+app.get("/get-users",authenticateToken,async(req,res) => {
+  const { user } = req.user;
+
+  const isUser = await User.findOne({ _id: user._id });
+
+  if(!isUser){
+      return res.sendStatus(401);
+  }
+
+  return res.json({
+      user:{
+          fullName: isUser.fullName,
+           email : isUser.email, 
+          _id : isUser._id,
+           createdOn:isUser.createdOn
+          },
+      message: "",
+  });
+});
+
+//Adding Foods
+app.post("/addfoods", async (req, res) => {
+  const { name, price, image, description } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ message: "Name is required" });
+  }
+
+  if (!price) {
+    return res.status(400).json({ message: "Price is required" });
+  }
+
+  if (!description) {
+    return res.status(400).json({ message: "Description is required" });
+  }
+
+  try {
+    const food = new Food({
+      name,
+      price,
+      image,
+      description,
+    });
+
+    await food.save();
+
+    res.status(201).json({
+      message: "Food item successfully added",
+      food,
+    });
+  } catch (error) {
+    console.error("Error saving food item:", error);
+    res.status(500).json({ message: "Error saving food item", error });
+  }
+});
+
+//Get Foods
+app.get("/fooditems", async (req, res) => {
+  try {
+    const foods = await Food.find();
+    res.json(foods);
+  } catch (error) {
+    console.error("Error fetching food items:", error);
+    res.status(500).json({ message: "Error fetching food items", error });
+  }
+});
+
 
 
 app.listen(port, () => {
