838d173cb3d4b30e13e55d890023abe8f723236e
anoopghm <140532297+anoopghm@users.noreply.github.com>
2025-02-23 12:30:31 +0530

    Changed to original URl

--- frontend/KhajaGhar/src/components/FoodDetails/FoodDetails.jsx
+++ frontend/KhajaGhar/src/components/FoodDetails/FoodDetails.jsx
@@ -12,7 +12,7 @@ const FoodDetails = () => {
 
   useEffect(() => {
     axios
-      .get("https://khaja-ghar-jlb3-i09nskm52-anoopghms-projects.vercel.app/fooditems")
+      .get("http://localhost:3000/fooditems")
       .then((response) => {
         setFoods(response.data);
       })
